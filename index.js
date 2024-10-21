e875d5bffe64be468262d834bc72b85f703ffc08
Richard <richard.soria@epn.edu.ec>
2024-10-21 15:50:21 -0500

    Clase 6

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const express = require('express') // Usar Express - COMMONJS
 
 const app = express() // Inicializar Express
 
-// Ruta /
+// Todas las solicitudes van a ser por json
+app.use(express.json()) 
+
+/*// Ruta / es un recurso que un servidor debe procesar atraves de un ente
 app.get('/', (req,res)=>{res.send("Hola")})
 
 // Ruta /franquicias
@@ -14,8 +17,116 @@ app.get('/franquicias', (req,res)=>{res.send("Información de franquicias")})
 // Ruta /simple
 app.get('/simple', (req,res)=>{res.send("Hamburguesa Simple")})
 
-// Middle
-app.use((req,res)=>{res.send("<h1>Page Not Found - 404</h1>")})
+app.get('/simple', (req,res)=>{res.send("Hamburguesa Simple")})
+
+// FORMAS EN LA QUE HAGA ALGO UN SERVIDOR
+
+// Cliente REST permite visualizar todo el flujo de la petición
+// Ruta /registrar
+app.post('/registrar', (req,res)=>{
+    const {email, password} =req.body
+    email === "richard@hotmail.com" ? res.send("User Register") : res.send("Bad credentials")
+    console.log(req.body)
+})
+
+// Se va aser un pedido através de los params
+// La ruta devulve un recurso
+
+// RUTA /products/:id               http://localhost:3000/products/99
+app.get('/products/:id',(req,res)=>{
+    const {id} = req.params
+
+    const products = [
+        {
+            id:56,
+            title:"Laptop",
+            price:5000
+        },
+        {
+            id:77,
+            title:"Play 5",
+            price:800
+        }
+    ]
+    // Casting +
+    const response = products.find((product)=>product.id === +id)
+
+    response ? res.send(response) : res.send("Product not found")
+})
+
+
+// Ruta /search
+app.get('/search',(req,res)=>{
+
+    // Destructuración
+    const {orden} = req.query
+
+    // Construcción de un objeto
+    const responses = {
+        pollo:"Pollo del Hornero",
+        milanesa:"Milanesa de pollo"
+    }
+
+    // Acceder a la propiedad del objeto 
+    const response = responses[orden] || "No existe"
+
+    res.send(response)
+})*/
+
+/*// TEXTO
+// Ruta /
+app.get("/",(req,res)=>{
+    res.send('Landing Page de bienvenida')
+})
+
+// JSON
+// Ruta /dashboard
+app.get("/dashboard",(req,res)=>{
+    const user = {
+        name:"Byron",
+        rol:"Admin"
+    }
+    res.send(user)
+})
+
+// IMAGEN
+// Ruta /user/profile
+app.get("/user/profile",(req,res)=>{
+    res.sendFile('./R.jpg',{
+        // Otorga el path del proyecto
+        root:__dirname
+    })
+})
+
+// Archvio
+// Ruta /user/settings
+app.get("/user/settings",(req,res)=>{
+    res.sendFile('./PRUEBA-1-GR1.pdf',{
+        // Otorga el path del proyecto
+        root:__dirname
+    })
+})
+
+// Página HTML
+// RUTA /404
+app.get('/mouse',(req,res)=>{
+    res.send('<h1>Page Not Found - 404</h1><button>Back to homepage</button>')
+})*/
+
+// MIDDLEWARE INTERMIDIARIO QUE VA A INTERCEPTAR TODAS LAS SOLICITUDES QUE INGRESEN AL SERVIDOR
+app.use((req,res,next)=>{
+    const {token} = req.body
+    const response = token === "123" ? true : false
+    response ? next() : res.status(401).json({msg:"Invalid Token"})
+})
+
+// RUTAS
+app.get("/gifs",(req,res)=>{
+    res.send("Lista de gifs")
+})
+
+
+//app.use((req,res)=>{res.send("<h1>Page Not Found - 404</h1>")})
 
 app.listen(3000)
 
---
+++ notes.js
@@ -0,0 +1,17 @@
+// Recursos Agnostico
+// Request = La solicitud que el servidor debe procesar
+// Response = El tipo (5 formas) de recurso que el servidor va a responder
+// 
+
+// ruta
+// header
+// body
+
+// Comprender errores
+// Ayuda al equipo
+// Aprendizaje mediante la enseñanza
+// Apreder de los errores
+
+// ETAPA 1 - FUNDAMENTOS AGNOSTICOS
+// ETAPA 2 - PROYECTO 100% BACKEND
+// ETAPA 3 - PROYECTO FULLSTACK
